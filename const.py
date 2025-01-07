9105fac638247dec508c210e4d7e84064240a7ec
kaaspad <kaaspad@gmail.com>
2025-01-07 16:22:37 +0200

    lots of missing stuff very odd

--- const.py
+++ const.py
@@ -1,3 +1,12 @@
+"""Constants for the Lutron Homeworks integration."""
+
+from __future__ import annotations
+
+DOMAIN = "homeworks"
+
+CONF_ADDR = "addr"
+CONF_BUTTONS = "buttons"
+CONF_CONTROLLER_ID = "controller_id"
 CONF_DIMMERS = "dimmers"
 CONF_INDEX = "index"
 CONF_KEYPADS = "keypads"
@@ -5,3 +14,9 @@ CONF_SWITCHES = "switches"
 CONF_LED = "led"
 CONF_NUMBER = "number"
 CONF_RATE = "rate"
+CONF_RELEASE_DELAY = "release_delay"
+
+DEFAULT_BUTTON_NAME = "Homeworks button"
+DEFAULT_KEYPAD_NAME = "Homeworks keypad"
+DEFAULT_LIGHT_NAME = "Homeworks light"
+DEFAULT_SWITCH_NAME = "Homeworks switch"
